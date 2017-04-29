5b9f5453402a782524bd6773db0aa42bbe5f8ef8
Mithi Sevilla <mithi.sevilla@gmail.com>
2017-04-29 16:57:40 +0800

    feat: Add get_weight()
    Returns the relative importance of a particle given its sensor
    measurements, the sensor measurement of another particles we are
    interested in localizing, and a noise factor.

--- particle_filter_prototype.py
+++ particle_filter_prototype.py
@@ -3,6 +3,18 @@ from copy import deepcopy
 from random import random, gauss
 from math import pi, sin, cos, sqrt, exp
 import numpy as np
+def gaussian_prob(mu, sigma, x): 
+    # calculate the probability of x for 1-dim Gaussian with mean mu and var sigma 
+    return exp( -((mu - x) ** 2) / (sigma ** 2) / 2.) / sqrt( 2. * pi * (sigma ** 2))    
+
+def get_weight(my_measurements, ground_measurements, noise):   
+
+  w = 1.
+  for my_distance, ground_distance in zip(my_measurements, ground_measurements):
+    w *= gaussian_prob(mu = my_distance, sigma = noise, x = ground_distance)
+  
+  return w + 1.e-300 # avoid round-off to zero
+
 def get_random_position(world_size):
     
   x = random() * world_size
