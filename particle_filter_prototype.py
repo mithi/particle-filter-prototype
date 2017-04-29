8e77f6f4afd6ca4ac38ecca9037fb234a6146d5b
Mithi Sevilla <mithi.sevilla@gmail.com>
2017-04-29 17:01:15 +0800

    feat: Add particle class

--- particle_filter_prototype.py
+++ particle_filter_prototype.py
@@ -37,3 +37,50 @@ def evaluate(robot, particles):
     s += sqrt((px - rx) ** 2 + (py - ry) ** 2)
   
   return s / len(particles)
+class Particle:
+    
+  def __init__(self, position = [0., 0., 0.], noises = [0., 0., 0.]): 
+    
+    forward_noise, turn_noise, sense_noise = noises
+    self.set_noise(forward_noise, turn_noise, sense_noise)
+    
+    x, y, heading = position
+    self.set_position(x, y, heading)
+      
+  def set_position(self, x, y, heading): 
+    heading %= (2. * pi)        
+    self.x, self.y, self.heading = x, y, heading
+    
+  def set_noise(self, forward_noise, turn_noise, sense_noise):
+    self.forward_noise = forward_noise
+    self.turn_noise = turn_noise
+    self.sense_noise = sense_noise
+
+  def forward(self, d):
+    distance = d + gauss(0., self.forward_noise)
+    self.x = (self.x + distance * cos(self.heading)) 
+    self.y = (self.y + distance * sin(self.heading))
+    
+  def turn(self, angle):
+    self.heading += (angle + gauss(0., self.turn_noise))
+    self.heading %= (2 * pi)
+               
+  def move(self, angle, distance):
+    self.turn(angle)
+    self.forward(distance)
+  
+  def sense(self, landmarks, with_noise = True):
+        
+    distances = []
+    
+    for landmark in landmarks:
+      d = distance(self.x, self.y, landmark.x, landmark.y)
+      if with_noise: d += gauss(0., self.sense_noise)
+      distances.append(d)
+    
+    return distances
+
+  def get_current_position(self):
+    return [self.x, self.y, self.heading]
+    
+
