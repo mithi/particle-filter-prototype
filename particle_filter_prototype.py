96cde07301a568ea19bf8e1b0d016420e2341002
Mithi Sevilla <mithi.sevilla@gmail.com>
2017-04-29 16:55:42 +0800

    feat: Add function get_random_position()
    returns a random x, y coordinate and heading (in radians) given the
    size of the world

--- particle_filter_prototype.py
+++ particle_filter_prototype.py
@@ -3,3 +3,11 @@ from copy import deepcopy
 from random import random, gauss
 from math import pi, sin, cos, sqrt, exp
 import numpy as np
+def get_random_position(world_size):
+    
+  x = random() * world_size
+  y = random() * world_size
+  heading =  random() * 2. * pi 
+
+  return [x, y, heading]
+
